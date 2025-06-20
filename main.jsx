34cd4691f39ed9d12cfcf193fe2d7ab5f5456c83
mloynd <mike.loynd@councilfire.ai>
2025-06-20 19:11:24 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Chat from './Chat.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Chat />
-  </React.StrictMode>
-)
